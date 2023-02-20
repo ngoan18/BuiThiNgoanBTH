5fe8360f58149a08b683efd0183173c57737fda1
Buikimngoan240@gmail.com@example.com <Buikimngoan240@gmail.com@example.com>
2023-02-20 16:24:36 +0700

    Ngay 20/02

---
+++ BuiThiNgoanBTH/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiThiNgoanBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
