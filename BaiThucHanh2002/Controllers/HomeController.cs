84fa9824d95b1eec8af6127ad6dbbe0178d31736
Buikimngoan240@gmail.com@example.com <Buikimngoan240@gmail.com@example.com>
2023-02-22 22:55:15 +0700

    ỵufyif

--- BaiThucHanh2002/Controllers/HomeController.cs
+++ BaiThucHanh2002/Controllers/HomeController.cs
@@ -12,10 +12,6 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-    public IActionResult BaiThucHanh2202()
-    {
-        return View();
-    }
     public IActionResult Index()
     {
         return View();
