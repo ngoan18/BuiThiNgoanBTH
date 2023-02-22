2089d8d48262819f73e6efa2369e7d1e5ef71abc
Buikimngoan240@gmail.com@example.com <Buikimngoan240@gmail.com@example.com>
2023-02-22 20:51:07 +0700

    hjj

---
+++ BaiThucHanh2002/Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BaiThucHanh2002.Models;
+namespace BaiThucHanh2002.Controllers
+{
+    public class TeacherController : Controller  
+    {
+         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+     }
+
+}
